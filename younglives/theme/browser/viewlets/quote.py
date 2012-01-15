e6fd240dc7e11cc57c91f48206703aec740037c5
Michael Davis <m.r.davis@me.com>
2012-01-15 17:46:56 +0000

    Add the quote to the non homepage pages

---
+++ younglives/theme/browser/viewlets/quote.py
@@ -0,0 +1,17 @@
+from zope.component import getMultiAdapter
+
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.app.layout.viewlets import ViewletBase
+
+class QuoteView(ViewletBase):
+
+    index = ViewPageTemplateFile("quote.pt")
+
+    def update(self):
+        portal_catalog = getToolByName(self.context, 'portal_catalog')
+        results = portal_catalog(meta_type='HomePage')
+        if results:
+            object = results[0].getObject()
+            self.quote = object.getField('homeQuote').get(object)
